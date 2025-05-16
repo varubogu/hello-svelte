82e8caa7f24dd123483e5952a7f63436dab4f177
varubogu <toyosuke@varubogu.com>
2025-05-16 08:47:36 +0000

    feat: アプリタイトルにアイコンを追加

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -3,5 +3,8 @@
 </script>
 
 <header class="mb-8">
-    <h1 class="text-3xl font-bold text-gray-800">{title}</h1>
+    <h1 class="flex items-center text-3xl font-bold text-gray-800">
+        <img src="/favicon.svg" alt="icon" class="w-20 h-20 inline-block align-middle" />
+        {title}
+    </h1>
 </header>
